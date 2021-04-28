21bdbe19fecff06dd1882560acad08df0809f197
Walfal <raphael.durandy@isep.fr>
2021-04-28 11:34:22 +0200

    Mise en page gererCapteurs

--- view/gererCapteurs/gererCapteurs.php
+++ view/gererCapteurs/gererCapteurs.php
@@ -11,7 +11,7 @@ require '../headerFooter/header.php'; ?>
 
 <div class="main">
 	<div class="blocCapteur">
-		<img  class ="temperature" src="../images/divers/Temperature.png" alt="capteur Température">
+		<img  class ="icon temperature" src="../images/divers/Temperature.png" alt="capteur Température">
 		<div class="capteur capteur1">
 			<p class = "nomCapteur">Capteur de température : </p><br>
 			<p class="miseJour">Dernière mise à jour : JJ/MM/AAAA</p>
@@ -20,28 +20,34 @@ require '../headerFooter/header.php'; ?>
 		</div>
 	</div>
 
-	<img class =" tonalite" src="../images/divers/hautParleur.png" alt="capteur de tonalité">
-	<div class="capteur capteur2">
-		<p class = "nomCapteur">Capteur de tonalité :</p><br>
-		<p class="miseJour">Dernière mise à jour : JJ/MM/AAAA</p>
-		<input  class="modifier" type="submit" name="modifier" id="modifier" value="Modifier">
-		<input  class="supprimer" type="submit" name="supprimer" id="supprimer" value="Supprimer">
+	<div class="blocCapteur">
+		<img class ="icon tonalite" src="../images/divers/hautParleur.png" alt="capteur de tonalité">
+		<div class="capteur capteur2">
+			<p class = "nomCapteur">Capteur de tonalité :</p><br>
+			<p class="miseJour">Dernière mise à jour : JJ/MM/AAAA</p>
+			<input  class="modifier" type="submit" name="modifier" id="modifier" value="Modifier">
+			<input  class="supprimer" type="submit" name="supprimer" id="supprimer" value="Supprimer">
+		</div>
 	</div>
 
-	<img class ="frequenceCardiaque" src="../images/divers/frequenceCardiaque.jpg" alt="capteur fréquence cardiaque">
-	<div class="capteur capteur3">
-		<p class = "nomCapteur">Capteur de fréquence cardiaque : </p><br>
-		<p class="miseJour">Dernière mise à jour : JJ/MM/AAAA</p>
-		<input  class="modifier" type="submit" name="modifier" id="modifier" value="Modifier">
-		<input  class="supprimer" type="submit" name="supprimer" id="supprimer" value="Supprimer">
+	<div class="blocCapteur">
+		<img class ="icon frequenceCardiaque" src="../images/divers/frequenceCardiaque.jpg" alt="capteur fréquence cardiaque">
+		<div class="capteur capteur3">
+			<p class = "nomCapteur">Capteur de fréquence cardiaque : </p><br>
+			<p class="miseJour">Dernière mise à jour : JJ/MM/AAAA</p>
+			<input  class="modifier" type="submit" name="modifier" id="modifier" value="Modifier">
+			<input  class="supprimer" type="submit" name="supprimer" id="supprimer" value="Supprimer">
+		</div>
 	</div>
 
-	<img class="tempsReaction" src="../images/divers/tempsReaction.jpg" alt="capteur / bouton mesure du temps de réaction">
-	<div class="capteur capteur4">
-		<p class = "nomCapteur">Capteur du temps de réaction : </p><br>
-		<p class = "miseJour">Dernière mise à Jour : JJ/MM/AAAA</p>
-		<input  class="modifier" type="submit" name="modifier" id="modifier" value="Modifier">
-		<input  class="supprimer" type="submit" name="supprimer" id="supprimer" value="Supprimer">
+	<div class="blocCapteur">
+		<img class="icon tempsReaction" src="../images/divers/tempsReaction.jpg" alt="capteur / bouton mesure du temps de réaction">
+		<div class="capteur capteur4">
+			<p class = "nomCapteur">Capteur du temps de réaction : </p><br>
+			<p class = "miseJour">Dernière mise à Jour : JJ/MM/AAAA</p>
+			<input  class="modifier" type="submit" name="modifier" id="modifier" value="Modifier">
+			<input  class="supprimer" type="submit" name="supprimer" id="supprimer" value="Supprimer">
+		</div>
 	</div>
 </div>
 
