b66950b59f96fce5756ce41fbf4a8391cf10632b
jad <www.haririjad@outlook.fr>
2021-05-19 12:50:20 +0200

    fin graph double axe

--- controller/resultats/graphResultats.php
+++ controller/resultats/graphResultats.php
@@ -1,4 +1,5 @@
 <?php // content="text/plain; charset=utf-8"
+
 session_start();
 require_once('../../jpgraph/src/jpgraph.php');
 require_once('../../jpgraph/src/jpgraph_line.php');
@@ -8,75 +9,83 @@ require_once('../../model/BDD/connexionBDD.php');
 
 $matricule = (isset($_GET['id']) && $_SESSION['matricule'] < 20) ? $_GET['id'] : $_SESSION['matricule'];
 $req = query($BDD, "SELECT debut, resultat, idCapteur FROM Test NATURAL JOIN PriseRDV WHERE matricule = ? AND idCapteur = ? ORDER BY debut", [$matricule, 0]);
+$frequenceCardiaque = $req->fetchAll();
+
+$req = query($BDD, "SELECT debut, resultat, idCapteur FROM Test NATURAL JOIN PriseRDV WHERE matricule = ? AND idCapteur = ? ORDER BY debut", [$matricule, 3]);
+$temperature = $req->fetchAll();
 
 
-$tests = $req->fetchAll();
-
-
-foreach($tests as $test){
+foreach($frequenceCardiaque as $test){
     $date = new DateTime($test['debut']);
     $dates[] = $date->format("d-m-Y");
-    $valeurs[] = $test["resultat"];
+    $resultatCardio[] = $test["resultat"];
 }
 
-//bar1
-$data1y=$valeurs;
-//bar2
-$data2y=array(180,200,220,190,170,195,190,210,200,205,195,150);
+
+foreach($temperature as $test){
+    //$date = new DateTime($test['debut']);
+    //$dates[] = $date->format("d-m-Y");
+    $resultatTemperature[] = $test["resultat"];
+}
 
 
-// Create the graph. These two calls are always required
-$graph = new Graph(750,320,'auto');
+$datay1 = $resultatCardio;
+$datay2 = $resultatTemperature;
+
+
+// Setup the graph
+$graph = new Graph(400,250);
 $graph->SetScale("textlin");
-$graph->SetY2Scale("lin",0,90);
-$graph->SetY2OrderBack(false);
 
 $theme_class = new UniversalTheme;
+
 $graph->SetTheme($theme_class);
-
-$graph->SetMargin(40,20,46,80);
-
-$graph->yaxis->SetTickPositions(array(0,50,100,150,200,250,300,350), array(25,75,125,175,275,325));
-//$graph->y2axis->SetTickPositions(array(30,40,50,60,70,80,90));
-
+$graph->img->SetAntiAliasing(false);
+$graph->title->Set('Temps de réaction');
 $graph->SetBox(false);
 
-$graph->ygrid->SetFill(false);
+$graph->SetMargin(40,20,36,63);
+
+$graph->img->SetAntiAliasing();
+
+$graph->yaxis->HideZeroLabel();
 $graph->yaxis->HideLine(false);
 $graph->yaxis->HideTicks(false,false);
-$graph->xaxis->SetTickLabels($dates[0]);
-
-// Create the bar plots
-$b1plot = new BarPlot($data1y);
-$b2plot = new BarPlot($data2y);
-
-
-$gbplot = new GroupBarPlot(array($b1plot,$b2plot));
-
-// ...and add it to the graPH
-$graph->Add($gbplot);
+//$graph->yaxis->scale->SetAutoMin(20);
+//$graph->y2scale->SetAutoMax(120);
+$graph->SetScale('textlin',20,120);
+$graph->yaxis->SetColor('blue','blue');
 
 
-$b1plot->SetColor("#0000CD");
-$b1plot->SetFillColor("#0000CD");
-$b1plot->SetLegend("Clients");
 
-$b2plot->SetColor("#B0C4DE");
-$b2plot->SetFillColor("#B0C4DE");
-$b2plot->SetLegend("Machines");
 
+
+$graph->xgrid->Show();
+$graph->xgrid->SetLineStyle("solid");
+$graph->xaxis->SetTickLabels($dates);
+$graph->xgrid->SetColor('#E3E3E3');
+
+// Create the first line
+$p1 = new LinePlot($datay1);
+$graph->Add($p1);
+$p1->SetColor("#6495ED");
+$p1->SetLegend('Cardiaque');
+
+
+
+// Create the second line
+$graph->SetYScale(0,'lin');
+$p2 = new LinePlot($datay2);
+$graph->AddY(0,$p2);
+$graph->ynaxis[0]->SetColor('red');
+$graph->ynaxis[0]->scale->SetAutoMax(30);
+$graph->ynaxis[0]->scale->SetAutoMin(10);
+$p2->SetColor("#B22222");
+$p2->SetLegend('Température');
 
 $graph->legend->SetFrameWeight(1);
-$graph->legend->SetColumns(6);
-$graph->legend->SetColor('#4E4E4E','#00A78A');
 
-$band = new PlotBand(VERTICAL,BAND_RDIAG,11,"max",'khaki4');
-$band->ShowFrame(true);
-$band->SetOrder(DEPTH_BACK);
-$graph->Add($band);
-
-$graph->title->Set("Combined Line and Bar plots");
-
-// Display the graph
+// Output line
 $graph->Stroke();
+
 ?>
